8df3b15a56072ed205dd3ba181fac9eff50bd977
cankush625 <cankush625@gmail.com>
2021-02-17 22:30:01 +0530

    Code cleaned
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- lib/widgets/appbar/appbar.dart
+++ lib/widgets/appbar/appbar.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 
 import 'package:neuralc/widgets/drawer/drawer.dart';
 
 // ignore: non_constant_identifier_names
 Widget Appbar(BuildContext context) {
-  var screenSize = MediaQuery.of(context).size;
   return AppBar(
     automaticallyImplyLeading: false,
     title: Row(
