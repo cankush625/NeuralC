5914296fda8dfd573414c9d2fc8207f740c0eeef
cankush625 <cankush625@gmail.com>
2021-02-22 12:28:35 +0530

    Features screen is added
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ lib/screens/general/features.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:neuralc/widgets/appbar/appbar.dart';
+import 'package:neuralc/widgets/drawer/drawer.dart';
+
+class Features extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Appbar(context),
+      drawer: AppBarDrawer(context),
+      body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(left: 1.0.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 2.0.h,
+                ),
+                Text(
+                  'Features: ',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                SizedBox(
+                  height: 2.0.h,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '1. ',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        'Generate captions for an image.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.0.h,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '2. ',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        'Supports images from camera as well as gallery.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.0.h,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '3. ',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        'Real time caption generation.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.0.h,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '4. ',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        'Speak captions from device speakers using text-to-speech.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.0.h,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '5. ',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        'High availability of service.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+      ),
+    );
+  }
+}
--- lib/widgets/drawer/drawer.dart
+++ lib/widgets/drawer/drawer.dart
@@ -24,7 +24,7 @@ Widget AppBarDrawer(BuildContext context) {
               style: Theme.of(context).textTheme.bodyText1,
             ),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.pushNamed(context, 'features');
             },
           ),
           ListTile(
